8eb80b18c5dea669ff0117a65168a181fa868cdd
Manas Ranjan Manna <madanmanna123456@gmail.com>
2023-07-25 17:14:48 +0530

    added readme.md file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/',function(req,res){
+    res.end('<h1>Everything good.You can checkout the routes now</h1>')
+})
+
 //forwarding all api related routes
 router.use('/api',require('./api'));
 
